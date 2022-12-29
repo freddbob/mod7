201e89fe47614d2b36b0d39d812bc178a2c338a5
freddbob <121595549+freddbob@users.noreply.github.com>
2022-12-29 09:14:49 -0600

    Update grundge.lua

--- ui_cache/grundge.lua
+++ ui_cache/grundge.lua
@@ -2,49 +2,31 @@ return function(_, CanEntityKill)
 	local Creator = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors%20Entity%20Spawner/Source.lua"))()
 	-- Create entity
 	local entity = Creator.createEntity({
-		CustomName = "Grundge", -- Custom name of your entity
-	   Model = "https://github.com/freddbob/mod6/blob/main/FireBrand3.rbxm?raw=true", -- Can be GitHub file or rbxassetid
-		Speed = 1000, -- Percentage, 100 = default Rush speed
-		DelayTime = 4, -- Time before starting cycles (seconds)
-		HeightOffset = 0,
-		CanKill = CanEntityKill,
-		BreakLights = true,
-		FlickerLights = {
-			true, -- Enabled
-			10, -- Time (seconds)
-		},
-		Cycles = {
-			Min = 15,
-			Max = 25,
-			WaitTime = 4,
-		 },
-		CamShake = {
-			true, -- Enabled
-			{5, 15, 0.1, 1}, -- Shake values (don't change if you don't know)
-			30, -- Shake start distance (from Entity to you)
-		},
-		   Jumpscare = {
-			true, -- Enabled ('false' if you don't want jumpscare)
-			{
-				Image1 = "rbxassetid://11482510186", -- Image1 url
-				Image2 = "rbxassetid://11482510186", -- Image2 url
-				Shake = true,
-				Sound1 = {
-					0, -- SoundId
-					 { Volume = 0.5 }, -- Sound properties
-				},
-				Sound2 = {
-					9125712561, -- SoundId
-					{ Volume = 0.5 }, -- Sound properties
-				},
-				Flashing = {
-						true, -- Enabled
-						Color3.fromRGB(1, 1, 255), -- Color
-					},
-				Tease = {
-						true, -- Enabled ('false' if you don't want tease)
-						Min = 3,
-						Max = 4,
+        Model = "https://github.com/freddbob/mod6/blob/main/FireBrand3.rbxm?raw=true",
+        Speed = 400,
+        DelayTime = 2,
+        HeightOffset = 0,
+        CanKill = true,
+        BreakLights = true,
+        FlickerLights = {
+            true,
+            1,
+        },
+        Cycles = {
+            Min = 2,
+            Max = 2,
+            WaitTime = 2,
+        },
+        CamShake = {
+            true,
+            {5, 15, 0.1, 1},
+            100,
+        },
+    })
+
+    Creator.runEntity(entity)
+end)
+
 					},
 			   },
 		},
