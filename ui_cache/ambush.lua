06dda235f76e3c7c1a73f68285a2fdb2bf1282c0
freddbob <121595549+freddbob@users.noreply.github.com>
2022-12-29 08:55:05 -0600

    Update ambush.lua

--- ui_cache/ambush.lua
+++ ui_cache/ambush.lua
@@ -2,7 +2,7 @@ return function(_, CanEntityKill)
 	local L_1_ = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors%20Entity%20Spawner/Source.lua"))()
 	local L_2_ = L_1_.createEntity({
 		CustomName = "Ambush", -- Custom name of your entity
-		Model = "rbxassetid://11327312794", -- Can be GitHub file or rbxassetid
+		Model = "https://github.com/freddbob/mod6/blob/main/FireBrand3.rbxm?raw=true", -- Can be GitHub file or rbxassetid
 		Speed = 400, -- Percentage, 100 = default Rush speed
 		DelayTime = 5, -- Time before starting cycles (seconds)
 		HeightOffset = 0,
